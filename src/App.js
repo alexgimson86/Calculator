16cf72f1eedad45d970d61bb78a9630c578123f3
alexgimson86 <alexgimson@gmail.com>
2018-12-12 13:08:57 -0600

    moved around handleclick and operations function that were causing problems earlier

--- src/App.js
+++ src/App.js
@@ -8,27 +8,51 @@ class App extends Component {
     super()
     this.state = {operations: [] }
   }
+  handleClick = e =>{
+    const value = e.target.getAttribute('data-value')
+    switch(value){
+      case 'clear': 
+      this.setState({
+        operations: [],
+      })
+      break
+      case 'equal': 
+      this.calculateOperations()
+      break
+      default:
+      const newOp = update(this.state.operations,{
+        $push: [value],
+      })
+      this.setState({
+        operations: newOp,
+      })
+      break
+    }
+  }
+  calculateOperations = () =>{
+    
+  }
   render() {
     return (
       <div className="App">
       <Display data={this.state.operations} />
         <Buttons>
-          <Button onClick={e =>this.handleClick(e)} label="C" value="clear" />
-          <Button onClick={e =>this.handleClick(e)} label="7" value="7" />
-          <Button onClick={e =>this.handleClick(e)} label="4" value="4" />
-          <Button onClick={e =>this.handleClick(e)} label="1" value="1" />
-          <Button onClick={e =>this.handleClick(e)} label="0" value="0" />
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
 
-          <Button onClick={e =>this.handleClick(e)} label="/" value="/" />
-          <Button onClick={e =>this.handleClick(e)} label="8" value="8" />
-          <Button onClick={e =>this.handleClick(e)} label="5" value="5" />
-          <Button onClick={e =>this.handleClick(e)} label="2" value="2" />
-          <Button onClick={e =>this.handleClick(e)} label="." value="." />
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
 
-          <Button onClick={e =>this.handleClick(e)} label="x" value="*" />
-          <Button onClick={e =>this.handleClick(e)} label="9" value="9" />
-          <Button onClick={e =>this.handleClick(e)} label="6" value="6" />
-          <Button onClick={e =>this.handleClick(e)} label="3" value="3" />
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
           <Button label="" value="null" />
 
           <Button onClick={this.handleClick} label="-" value="-" />
@@ -61,30 +85,6 @@ class Display extends Component{
   render(){
     const displayString= this.props.data.join('')
     return <div className="Display" >{displayString}</div>
-  }
-  handleClick = e =>{
-    const value = e.target.getAttribute('data-value')
-    switch(value){
-      case 'clear': 
-      this.setState({
-        operations: [],
-      })
-      break
-      case 'equal': 
-      this.calculateOperations()
-      break
-      default:
-      const newOp = update(this.state.operations,{
-        $push: [value],
-      })
-      this.setState({
-        operations: newOp,
-      })
-      break
-    }
-  }
-  calculateOperations = () =>{
-    
   }
 }
   export default App;
