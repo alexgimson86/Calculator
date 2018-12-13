24a9ab68f2fae9836350fd42a0443f06cfc7958c
alexgimson86 <alexgimson@gmail.com>
2018-12-13 16:46:00 -0600

    fixed some logic issues in handle click function

--- src/App.js
+++ src/App.js
@@ -2,67 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import update from 'immutability-helper';
-const math = require('mathjs')
+import math from 'mathjs';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {operations: [], answer: null, previousButton: null };
+    this.state = {operations: []};
     this.handleClick = this.handleClick.bind(this);
     this.calculateOperations = this.calculateOperations.bind(this);
   }
   handleClick = e =>{
     var value = e.target.getAttribute('data-value')
-    var valArr = []
-
-    
-    var keypress = 1
-    if(this.state.answer !== null && value !== 'clear'){
-      keypress = 2
-      if(value !== '-' && value !== '+' && value !== '*' && value !== '/')
-        valArr.push('clear')
-      else{
-        keypress = 3
-        valArr.push('clear')
-        valArr.push(this.state.answer)
-      }
-    }
-    valArr.push(value);
-    for(let i = 0; i < keypress;i++){
-      switch(valArr[i]){
-        case 'clear': 
-        this.setState((state)=>{
-          return { operations: [],answer: null, previousButton: valArr[i]}
+      switch(value){
+        case "clear": 
+        this.setState({
+           operations: []
         });
         break
-        case 'equal': 
-        this.setState({
-          previousButton: valArr[i]
-        });
-        this.calculateOperations()
+        case "equal": 
+          this.calculateOperations()
         break
         default:
         var newOp = this.state.operations.slice();
-        newOp.push(valArr[i])
+        newOp.push(value)
         this.setState({
-          operations: newOp,
-          previousButton: valArr[i]
+          operations: newOp
         });
-       
         break
       }
   }
-  }
   calculateOperations = () =>{
     let mathString = this.state.operations.join('')
-    this.setState((state)=>{
-      return {answer : math.eval(mathString)}   
+    this.setState({
+      operations : [math.eval(mathString)]  
     }) 
   }
   render() {
     return (
       <div className="App">
-      <Display data={this.state.operations} previousButton={this.state.previousButton} answer={this.state.answer} />
+      <Display data={this.state.operations}/>
         <Buttons>
           <Button onClick={this.handleClick} label="C" value="clear" />
           <Button onClick={this.handleClick} label="7" value="7" />
